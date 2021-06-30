6a85b79162b56cace9384abdc60b508d97e4e6bb
LHN2225 <50903203+LHN2225@users.noreply.github.com>
2021-06-30 17:53:24 +0700

    Add project files.

---
+++ NaniShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NaniShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
