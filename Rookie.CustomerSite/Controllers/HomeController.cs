f7e81720b507dcbab3bc94da35393d3cb813f449
LHN2225 <50903203+LHN2225@users.noreply.github.com>
2021-07-06 10:36:18 +0700

    add sample of success httpClient GET

--- Rookie.CustomerSite/Controllers/HomeController.cs
+++ Rookie.CustomerSite/Controllers/HomeController.cs
@@ -5,7 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using RookieShop.Shared;
 
 namespace Rookie.CustomerSite.Controllers
 {
@@ -23,9 +26,61 @@ namespace Rookie.CustomerSite.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult PrivacyAsync()
         {
-            return View();
+            //List<Product> products = GetProductAsync("http://localhost:44341/api/Products");
+            //return View(products);
+
+            /*using var client = new HttpClient();
+            var result = await client.GetAsync("http://localhost:44341/api/Products");
+            return View(result);*/
+
+            /* Product product = null;
+             using (var client = new HttpClient())
+             {
+                 client.BaseAddress = new Uri("http://localhost:51708");
+                 client.DefaultRequestHeaders.Accept.Clear();
+                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                 //GET Method  
+                 HttpResponseMessage response = await client.GetAsync("http://localhost:44341/api/Products/RSCAPM0001");
+                 if (response.IsSuccessStatusCode)
+                 {
+                     product = await response.Content.ReadAsAsync<Product>();
+                 }
+             }
+
+             if (product == null) return View(0);
+             return View(1);*/
+            Product product = GetProductAsync("api/Products/RSCAPWM0002").GetAwaiter().GetResult();
+            
+            if (product == null) return View(null);
+            return View(product);
+        }
+
+
+        static async Task RunAsync()
+        {
+
+        }
+
+        static async Task<Product> GetProductAsync(string path)
+        {
+
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://localhost:44341/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            Product product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Product>();
+            }
+            return product;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ RookieShop.Shared/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace RookieShop.Shared
+{
+    public class Product
+    {
+        public string id { get; set; }
+        public string imageUri { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int amount { get; set; }
+        public float price { get; set; }
+        public virtual Category category { get; set; }
+
+        public List<Rating> ratings { get; set; } = new List<Rating>();
+    }
+}
