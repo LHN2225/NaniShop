4cabf556e59f68b2cf322f91f00eda29344d8494
LHN2225 <50903203+LHN2225@users.noreply.github.com>
2021-07-23 09:53:49 +0700

    Added view rating list component

---
+++ Rookie.CustomerSite/ViewComponents/RatingListViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RookieShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rookie.CustomerSite.ViewComponents
+{
+	public class RatingListViewComponent : ViewComponent
+	{
+        public IViewComponentResult Invoke(List<RatingDto> ratingList, int totalDisplay)
+        {
+            dynamic target = new ExpandoObject();
+            target.ratinglist = ratingList;
+            target.total = totalDisplay;
+            return View(target);
+        }
+    }
+}
